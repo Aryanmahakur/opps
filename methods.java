a400310f4d82cf9498d03e3fbf36b88647cf1924
Aryanmahakur <mahakuraryan@gmail.com>
2025-03-25 16:14:11 +0530

    uf

---
+++ methods.java
@@ -0,0 +1,9 @@
+class methods{
+    void greet(){
+        System.out.println("hello");
+    }
+    public static void main(String[] args) {
+        methods obj = new methods();
+        obj.greet();
+    }
+}
